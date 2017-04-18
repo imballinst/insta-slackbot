c716b76346223e65192b9e3f468159baef06971b
Try Ajitiono <ballinst@gmail.com>
2017-04-18 18:30:18 +0700

    feat(libs): added libs for HttpRequest

---
+++ libs/HttpRequest.js
@@ -0,0 +1,25 @@
+const http = require('http');
+
+function sendRequest(options, data) {
+  const req = http.request(options, (res) => {
+    console.log(`STATUS: ${res.statusCode}`);
+    console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
+    res.setEncoding('utf8');
+    res.on('data', (chunk) => {
+      console.log(`BODY: ${chunk}`);
+    });
+    res.on('end', () => {
+      console.log('No more data in response.');
+    });
+  });
+
+  req.on('error', (e) => {
+    console.error(`problem with request: ${e.message}`);
+  });
+
+  // write data to request body
+  req.write(data);
+  req.end();
+}
+
+export default sendRequest;
